5878403bebc2da8cc933cf1a5aa7bd0ee7c15090
AveWycc220 <mik16748@gmail.com>
2020-05-04 19:28:38 +0300

    Add console-interface

--- duffing_equation.py
+++ duffing_equation.py
@@ -2,6 +2,7 @@
 import numpy as np
 from scipy import integrate
 from matplotlib import pyplot as plt
+import ast
 
 def diff_eq(_X, t):
     """ Differential equation """
@@ -10,17 +11,31 @@ def diff_eq(_X, t):
     _dy = gamma*np.cos(w*t) - alpha*x - beta*x**3 - delta*y
     return[_dx, _dy]
 
-alpha = 1   # spring ratio
-beta = 1         # nonlinear restoring force
-delta = 1     # damping
-gamma = 1      # amplitude
-w = 1          # frequency
-X = [np.random.uniform(0.5, 1), np.random.uniform(0.5, 1)]
-# Solve for the trajectories
-tspan = np.linspace(0, 500, 400000)
-x_t = integrate.odeint(diff_eq, X, tspan)
-dx = x_t[:,0]
-dy = x_t[:,1]
-lines = plt.plot(dx, dy, ms=1)
-plt.setp(lines, linewidth=0.5)
-plt.show()
+if __name__ == '__main__':
+    alpha = 1   # spring ratio
+    beta = 1         # nonlinear restoring force
+    delta = 1     # damping
+    gamma = 1      # amplitude
+    w = 1          # frequency
+    while True: 
+        print('1 -> Change factors, 2 -> Show oscillator_lines, q -> Quit')
+        choose = input()
+        if choose == '1':
+            alpha = ast.literal_eval(input("alpha = "))
+            beta = ast.literal_eval(input("beta = "))
+            delta = ast.literal_eval(input("delta = "))
+            gamma = ast.literal_eval(input("gamma = "))
+            w = ast.literal_eval(input("w = "))
+        if choose == '2':
+            X = [np.random.uniform(0.5, 1), np.random.uniform(0.5, 1)]
+            # Solve for the trajectories
+            tspan = np.linspace(0, 500, 400000)
+            x_t = integrate.odeint(diff_eq, X, tspan)
+            dx = x_t[:,0]
+            dy = x_t[:,1]
+            lines = plt.plot(dx, dy, ms=1)
+            plt.setp(lines, linewidth=0.5)
+            plt.title(F'alpha = {alpha} beta = {beta} delta = {delta} gamma = {gamma} w = {w}')
+            plt.show()
+        if choose == 'q':
+            break
